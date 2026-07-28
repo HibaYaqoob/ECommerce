2482b1c2b6f5dacfad8ebde2d6b81ca35592fb5e
HibaYaqoob <hebaalmusalami@gmail.com>
2026-07-28 13:23:27 +0400

    Creating The Order Product Class

---
+++ ECommerce/Models/OrderProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Models
+{
+    public class OrderProduct
+    {
+        // Composite Key
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        // Navigation Properties
+        public Order Order { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
