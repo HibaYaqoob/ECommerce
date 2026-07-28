6516fc51f1885997588f55df9d0193a459a28681
HibaYaqoob <hebaalmusalami@gmail.com>
2026-07-28 13:20:51 +0400

    Creating The Review Class

---
+++ ECommerce/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        // Foreign Key
+        public int OrderId { get; set; }
+
+        // Navigation Property
+        public Order Order { get; set; }
+    }
+}
