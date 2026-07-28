bf03c0b38e1fb7a008676c09732ed8e702fb4010
HibaYaqoob <hebaalmusalami@gmail.com>
2026-07-28 13:10:12 +0400

    Creating the User Class

---
+++ ECommerce/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        // Navigation Property
+        public List<Order> Orders { get; set; } = new List<Order>();
+    }
+}
